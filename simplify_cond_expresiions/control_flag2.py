d2c8e20f7202e9139fbf6ba3ce8ed6a45c66a024
noirvin <vin.sp97@googlemail.com>
2021-03-05 23:28:32 -0800

    cont flag 2 and 3

---
+++ simplify_cond_expresiions/control_flag2.py
@@ -0,0 +1,12 @@
+# by Kami Bigdely
+# Remove control flag
+# Reference: https://stackoverflow.com/a/10140333/81306
+# This code snippet reads up to the end of the file
+n = 16
+with open('foobar.file', 'rb') as fp:
+    for chunk in fp:
+        if chunk == '': # end of file, stop running.
+            break
+        else:
+            print(chunk)
+        # process(chunk)
