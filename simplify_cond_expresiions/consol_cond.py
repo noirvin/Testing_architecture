6b8355b8306bfc8cf6e0f9a0c35768dea4c8d257
noirvin <vin.sp97@googlemail.com>
2021-03-05 23:12:22 -0800

    consol cond

---
+++ simplify_cond_expresiions/consol_cond.py
@@ -0,0 +1,24 @@
+
+# by Kami Bigdely
+# Consolidate conditional expressions
+def dice(ingredients):
+    print("diced all ingredients.")
+def mix_all(diced_ingredients):
+    print("mixed all.")
+def add_salt():
+    print('added salt.')
+def pour(liquid):
+    print('poured', liquid + '.',)
+
+def make_shirazi_salad(ingredients):
+    if 'cucumber' not in ingredients or 'tomato' not in ingredients or 'onion' not in ingredients or 'lemon juice' not in ingredients:
+        print('lacks ingredients.')
+        return
+    dice(ingredients)
+    mix_all(ingredients)
+    add_salt()
+    pour('lemon juice')
+    print('Your yummy shirazi salad is ready!')
+
+if __name__ == "__main__":
+    make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
