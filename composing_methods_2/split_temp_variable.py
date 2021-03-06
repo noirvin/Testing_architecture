2f1ab6c12b070d364de961ef00a7bee988bddb48
noirvin <vin.sp97@googlemail.com>
2021-03-05 22:52:59 -0800

    split_temp

---
+++ composing_methods_2/split_temp_variable.py
@@ -0,0 +1,19 @@
+# by Kami Bigdely
+# Split temporary variable
+"""
+LEADING DOCSTRING
+"""
+PATTY = 70 # [gr]
+PICKLE = 20 # [gr]
+TOMATOES = 25 # [gr]
+LETTUCE = 15 # [gr]
+BUNS = 95 # [gr]
+SANDWICH_WEIGHT = (2 * PATTY + 4 * PICKLE + 3 * TOMATOES + 2 * LETTUCE
+                + 2 * BUNS)
+print("NY Burger Weight", SANDWICH_WEIGHT)
+KIMCHI = 30 # [gr]
+MAYO = 5 # [gr]
+GOLDEN_FRIED_ONION   = 20 # [gr]
+SANDWICH_WEIGHT = (2 * PATTY + 4 * PICKLE + 3 * TOMATOES
+                + KIMCHI + MAYO + GOLDEN_FRIED_ONION + 2 * BUNS)
+print("Seoul Kimchi Burger Weight", SANDWICH_WEIGHT)
