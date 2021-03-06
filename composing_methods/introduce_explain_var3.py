850f7972d31bec1d1d60ef45cb7cf09def1b1a6d
noirvin <vin.sp97@googlemail.com>
2021-03-05 22:21:47 -0800

    intro expl var3 refactored

---
+++ composing_methods/introduce_explain_var3.py
@@ -0,0 +1,12 @@
+# by Kami Bigdely
+# Extract Variable (alias introduce explaining variable)
+WELL_DONE = 900000
+MEDIUM = 600000
+COOKED_CONSTANT = 0.05
+
+def is_cookeding_criteria_satisfied(order):
+    if order.desired_state() == 'well-done' and order.time() * order.temperature() * order.pressure() * COOKED_CONSTANT >= WELL_DONE:
+        return True
+    if order.desired_state() == 'medium' and order.time() * order.temperature() * order.pressure() * COOKED_CONSTANT >= MEDIUM:
+        return True
+    return False
