404bfe204081a9b4ca39861747c25fd9bd3df641
noirvin <vin.sp97@googlemail.com>
2021-03-05 22:19:11 -0800

    fowler reactored

--- composing_methods/fowler.py
+++ composing_methods/fowler.py
@@ -2,11 +2,14 @@
 # Adapted from a Java code in the "Refactoring" book by Martin Fowler.
 # Replace temp with query
 # Code snippet. Not runnable.
-def get_price():
+def get_price(quantity,item_price):
     base_price = quantity * item_price
+    return base_price * get_discount(base_price)
+
+def get_discount(base_price):
     discount_factor = 0
     if base_price > 1000:
         discount_factor = 0.95
     else:
         discount_factor = 0.98
-    return base_price * discount_factor
+    return discount_factor
