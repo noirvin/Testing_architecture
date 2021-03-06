66b1042c735af6a68566c77a9f5c77a02b1f3898
noirvin <vin.sp97@googlemail.com>
2021-03-05 23:06:59 -0800

    spl_temp_var2

---
+++ composing_methods_2/split_temp_var2.py
@@ -0,0 +1,13 @@
+
+# By Kami Bigdely
+# Split temp variable
+
+def save_into_db(info):
+    print("Saved into database.")
+
+
+username = input('Please enter your username: ')
+save_into_db(username)
+birth_year = int(input('Please enter your birth year: '))
+age = 2020 - birth_year
+print("You are", age, "years old.")
